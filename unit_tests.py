57ff4db23195c95d1fcf8822e01e5e433c4effd5
jetixl <frapugna@gmail.com>
2023-06-08 17:55:34 +0200

    created empty unit test file

--- unit_tests.py
+++ unit_tests.py
@@ -3,7 +3,7 @@ import unittest
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FO')
+        self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
